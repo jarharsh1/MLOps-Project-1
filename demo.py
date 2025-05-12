fbf28fccc762131701e8ad0e1d1b25801e741687
Harsh <jarharsh11@gmail.com>
2025-05-12 08:52:53 +0530

    Data ingestion

--- demo.py
+++ demo.py
@@ -1,28 +1,30 @@
 # below code is to check the logging config
-from src.logger import logging
+# from src.logger import logging
 
-logging.debug("This is a debug message.")
-logging.info("This is an info message.")
-logging.warning("This is a warning message.")
-logging.error("This is an error message.")
-logging.critical("This is a critical message.")
+# logging.debug("This is a debug message.")
+# logging.info("This is an info message.")
+# logging.warning("This is a warning message.")
+# logging.error("This is an error message.")
+# logging.critical("This is a critical message.")
 
 # --------------------------------------------------------------------------------
 
 # below code is to check the exception config
-from src.logger import logging
-from src.exception import MyException
-import sys
+# from src.logger import logging
+# from src.exception import MyException
+# import sys
 
-try:
-    a = str(1)+'Z'
-except Exception as e:
-    logging.info(e)
-    raise MyException(e, sys) from e
+# try:
+#     a = str(1)+'Z'
+# except Exception as e:
+#     logging.info(e)
+#     raise MyException(e, sys) from e
 
 # --------------------------------------------------------------------------------
 
-# from src.pipline.training_pipeline import TrainPipeline
+from src.pipline.training_pipeline import TrainPipeline
 
-# pipline = TrainPipeline()
-# pipline.run_pipeline()
+pipline = TrainPipeline()
+pipline.run_pipeline()
+
+# mongodb+srv://jarharsh13:Titanhere@cluster0.purpvvd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
